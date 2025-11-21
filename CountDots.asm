a44ef61fc5105cf9dbee65a15bee347a70e92e97
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-21 11:34:13 -0500

    Implement CountDots procedure in CountDots.asm
    Added CountDots procedure to count dots on the board.

---
+++ CountDots.asm
@@ -0,0 +1,9 @@
+Include Tag.inc
+
+.code
+;This procedure counts the number of dots on the board.
+;If the value on the board is a '.' incrementation will occur
+CountDots PROC PUBLIC USES eax ebx ecx edx
+  ret
+CountDots ENDP
+END
