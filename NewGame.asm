aa9745f251aafc3304af2cff13e6bd33e1089ef6
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-23 17:45:45 -0500

    Set player starting positions in NewGame.asm
    Initialize player starting positions on game start.

--- NewGame.asm
+++ NewGame.asm
@@ -10,6 +10,12 @@ Init PROC PUBLIC
     ; Sets up the map once the game begins
     call ResetMap
 
+    ; Every time the game restarts or starts, these are the starting positions of the players
+    mov px, 1
+    mov py, 1
+    mov gx, mapW-2
+    mov gy, mapH-2
+
     ret
 Init ENDP
 
