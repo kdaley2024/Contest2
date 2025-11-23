bb09b872c00cdd5fe86968acf28a14ad3b0cb9b1
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-23 17:39:34 -0500

    Set up initial game state in NewGame.asm
    Initialize game settings with default lives and score.

--- NewGame.asm
+++ NewGame.asm
@@ -2,7 +2,14 @@ INCLUDE Tag.inc
 
 .code
 
+;This Procedure involves the setup of a new game
 Init PROC PUBLIC
+    mov lives, 3
+    mov score, 0
+
+    ; Sets up the map once the game begins
+    call ResetMap
+
     ret
 Init ENDP
 
