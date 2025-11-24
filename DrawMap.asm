996c8211259a6fcc51856398119764271c10d77c
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-24 11:29:10 -0500

    Update DrawMap.asm

--- DrawMap.asm
+++ DrawMap.asm
@@ -5,8 +5,19 @@ mapH        EQU 20
 MAPSIZE     EQU (mapW * mapH)
 
 .code
+;this procedure creates the map with the walls, the edible dots, as well as designing them with colors, etc
 DrawMap PROC PUBLIC USES eax ebx ecx edx esi edi
-  ret
+    xor edi, edi                 ; y = 0
+rowLoop:
+    cmp edi, mapH                
+    jge doneRows
+
+    xor esi, esi                 ; x = 0
+colLoop:
+    cmp esi, mapW                
+    jge nextRow
+doneRows:
+nextRow:
 DrawMap ENDP
 
 END
