9104db3d0c3771312177ab68e2b0d4db33b77d88
ChervelleP <cpierre2022@my.fit.edu>
2025-11-25 20:45:25 +0000

    Compute the new position y coordinates

--- PlayerTryMove.asm
+++ PlayerTryMove.asm
@@ -5,14 +5,24 @@ PlayerTryMove PROC PUBLIC USES ecx edx esi
     ; compute newX = px + dx
     mov  edx, px
     add  edx, eax
+
     ; clamp newX into [1, mapW-2]
     cmp  edx, 1
     jge  @chkRightX
     mov  edx, 1
     jmp  @xClamped
+
 @chkRightX:
-    
+    cmp  edx, mapW-2
+    jle  @xClamped
+    mov  edx, mapW-2
+
 @xClamped:
+    ; compute newY = py + dy
+    mov  ecx, py
+    add  ecx, ebx
+
+
     ret
 PlayerTryMove ENDP
 END
