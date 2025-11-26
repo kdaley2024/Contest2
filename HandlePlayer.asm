af9bb979967d4183a154b4b11391dc99cb05a448
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-26 17:59:54 -0500

    Define virtual keys for player movement
    Added virtual key definitions for player controls.

--- HandlePlayer.asm
+++ HandlePlayer.asm
@@ -1,4 +1,12 @@
 INCLUDE Tag.inc
+
+;virtual key definitions
+VK_LEFT     EQU 25h
+VK_UP       EQU 26h
+VK_RIGHT    EQU 27h
+VK_DOWN     EQU 28h
+VK_ESCAPE   EQU 1Bh
+
 .code
 
 HandlePlayer PROC PUBLIC USES eax ebx
