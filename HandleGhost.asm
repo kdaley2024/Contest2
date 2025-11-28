92d3d461beb93e6b6cda76dd15925cd2342b0b69
ChervelleP <cpierre2022@my.fit.edu>
2025-11-28 04:43:31 +0000

    Try to make helper procedure for moving instead

--- HandleGhost.asm
+++ HandleGhost.asm
@@ -1,14 +1,8 @@
 INCLUDE Tag.inc
+
 .code
 HandleGhost PROC PUBLIC USES eax ebx
-    push VK_A
-    call GetAsyncKeyState@4
-    test ax, 8000h
-    jz   @noA
-    mov  eax, -1
-    mov  ebx, 0
-    call GhostTryMove
-@noA:
+  
     ret
 HandleGhost ENDP
 END
