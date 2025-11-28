1cb9b76d44d37e0a403b0a144b1f5505ca2590a9
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-28 13:56:15 -0500

    Update HandleGhost.asm

--- HandleGhost.asm
+++ HandleGhost.asm
@@ -5,6 +5,11 @@ mapW        EQU 30
 mapH        EQU 20
 MAPSIZE     EQU (mapW * mapH)
 
+VK_A        EQU 41h   
+VK_D        EQU 44h   
+VK_W        EQU 57h   
+VK_S        EQU 53h   
+
 .code
 HandleGhost PROC PUBLIC USES eax ebx
   
