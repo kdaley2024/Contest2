158d1d9fab8a3e77b90709ac00ce426b596821f0
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-28 14:03:03 -0500

    Modify HandleGhost to include VK_A key check

--- HandleGhost.asm
+++ HandleGhost.asm
@@ -17,6 +17,11 @@ HandleGhost PROC PUBLIC USES eax ebx
     push VK_A
     call GetAsyncKeyState@4
     test ax, 8000h
+    jz   @noA
+    mov  eax, -1
+    mov  ebx, 0
+    call GhostTryMove
+@noA:
   
     ret
 HandleGhost ENDP
