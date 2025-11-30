0f00add11ca36eadfbc7a34f80e85f9ce95950d6
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-30 14:10:22 -0500

    Implement collision check in CheckCollisions.asm
    Add collision detection logic to CheckCollisions procedure.

--- CheckCollisions.asm
+++ CheckCollisions.asm
@@ -4,6 +4,13 @@ INCLUDE Tag.inc
 ;this procedure checks if the coordinates of C and G are equal and if they are
 ;there would be a collision and the life of C would decrease by 1 and the players would be reset to their respective coordinates.
 CheckCollisions PROC PUBLIC USES eax ecx
+
+  mov eax, px
+  mov ecx, gx
+  cmp eax, ecx
+  jne Ret
+
+Ret:
   ret
 CheckCollisions ENDP
 END
